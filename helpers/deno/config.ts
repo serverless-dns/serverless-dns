7b8d11f1d1d1b3f4cc1610e2118a9beca7740776
Amith Mohanan <amithm7@gmail.com>
2021-11-16 13:10:36 +0530

    refactor(deno): m. Set runtime_env in app config

--- helpers/deno/config.ts
+++ helpers/deno/config.ts
@@ -3,6 +3,7 @@ import { config as dotEnvConfig } from "dotenv";
 // Load env variables from .env file to Deno.env (if file exists)
 try {
   dotEnvConfig({ export: true });
+  Deno.env.set("RUNTIME_ENV", "deno");
 } catch (e) {
   // throws without --allow-read flag
   console.error(".env file may not be loaded => ", e.name, ":", e.message);
