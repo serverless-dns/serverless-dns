28af84cada1a1afb2648cc8fcc7b1ecf3930e543
santhosh-ponnusamy <67677652+santhosh-ponnusamy@users.noreply.github.com>
2021-11-20 22:33:54 +0530

    introduce cache for dns response

--- dnsResolver.js
+++ dnsResolver.js
@@ -6,8 +6,13 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+import DNSParserWrap from "./dnsParserWrap.js";
+import { LocalCache as LocalCache } from "@serverless-dns/cache-wrapper";
+
 export default class DNSResolver {
   constructor() {
+    this.dnsParser = new DNSParserWrap();
+    this.dnsResCache = false
   }
   /**
    * @param {*} param
@@ -15,6 +20,7 @@ export default class DNSResolver {
    * @param {ArrayBuffer} param.requestBodyBuffer
    * @param {String} param.dnsResolverUrl
    * @param {String} param.runTimeEnv
+   * @param {WorkerEvent} event
    * @returns
    */
   async RethinkModule(param) {
@@ -24,12 +30,16 @@ export default class DNSResolver {
     response.exceptionFrom = "";
     response.data = {};
     try {
-      response.data.responseBodyBuffer = await (await resolveDns(
-        param.request,
-        param.dnsResolverUrl,
-        param.requestBodyBuffer,
-        param.runTimeEnv,
-      )).arrayBuffer();
+      if (!this.dnsResCache) {
+        this.dnsResCache = new LocalCache(
+          "dns-response-cache",
+          2000,
+          500,
+          2,
+          param.runTimeEnv,
+        );
+      }
+      response.data.responseBodyBuffer = await checkCacheBfrResolve.call(this, param)
     } catch (e) {
       response.isException = true;
       response.exceptionStack = e.stack;
@@ -41,6 +51,74 @@ export default class DNSResolver {
     return response;
   }
 }
+
+/**
+ * @param {Object} param
+ * @returns
+ */
+async function checkCacheBfrResolve(param) {
+  let responseBodyBuffer
+  let decodedDnsPacket = await this.dnsParser.Decode(
+    param.requestBodyBuffer
+  );
+
+  const dn = (decodedDnsPacket.questions.length > 0 ? decodedDnsPacket.questions[0].name : "").trim().toLowerCase() + ":" + decodedDnsPacket.questions[0].type
+  let cacheRes = this.dnsResCache.Get(dn)
+  if (!cacheRes) {
+    //console.log("Not in Cache -> resolve and update")
+    cacheRes = {}
+    responseBodyBuffer = await resolveDnsUpdateCache.call(this, param, cacheRes, dn)
+  }
+  else {
+    let now = Date.now()
+    // 30sec grace time for expired cache entries
+    if (now <= (cacheRes.data.ttlEndTime + 30)) {
+      //console.log("Found in Cache with ttl - used from cache")
+      //console.log(now + "::" + cacheRes.data.ttlEndTime + "::diff::" + cacheRes.data.ttlEndTime - now)
+      decodedDnsPacket = cacheRes.data.decodedDnsPacket
+      if (decodedDnsPacket.answers.length > 0) {
+        //set 30sec grace time when ttl from cache is negative
+        decodedDnsPacket.answers[0].ttl = Math.max(Math.floor((cacheRes.data.ttlEndTime - now) / 1000), 30)
+      }
+      responseBodyBuffer = this.dnsParser.Encode(decodedDnsPacket)
+    }
+    else {
+      //console.log("Found in Cache with expired ttl -> resolve and update")
+      responseBodyBuffer = await resolveDnsUpdateCache.call(this, param, cacheRes, dn)
+    }
+  }
+  this.dnsResCache.Put(cacheRes, param.event, param.runTimeEnv);
+  return responseBodyBuffer
+}
+
+/**
+ * @param {Object} param
+ * @param {Object} cacheRes
+ * @param {String} dn
+ * @returns
+ */
+async function resolveDnsUpdateCache(param, cacheRes, dn) {
+  let responseBodyBuffer = await (await resolveDns(
+    param.request,
+    param.dnsResolverUrl,
+    param.requestBodyBuffer,
+    param.runTimeEnv,
+  )).arrayBuffer();
+
+  let decodedDnsPacket = await this.dnsParser.Decode(
+    responseBodyBuffer
+  );
+  // min 60sec ttl for single answers and 300sec for multi or no answer
+  let ttl = decodedDnsPacket.answers.length == 1 ? Math.max(decodedDnsPacket.answers[0].ttl, 60) : 300 // todo - check all answers to find min ttl
+
+  cacheRes.k = dn
+  cacheRes.data = {}
+  cacheRes.data.decodedDnsPacket = decodedDnsPacket
+  cacheRes.data.ttlEndTime = (ttl * 1000) + Date.now()
+  //console.log(JSON.stringify(cacheRes))
+  return responseBodyBuffer
+}
+
 
 /**
  * @param {Request} request
@@ -59,13 +137,7 @@ async function resolveDns(request, resolverUrl, requestBodyBuffer, runTimeEnv) {
     u.protocol = dnsResolverUrl.protocol; // override proto, default https
 
     const headers = { // FIXME: are these headers needed? ~ mz
-      "crossDomain": "true",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers":
-        "X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers",
-      "Access-Control-Allow-Methods": "POST, GET, PUT, OPTIONS, DELETE",
-      "Content-Type": "application/dns-message",
-      "accept": "application/dns-message",
+      "Content-Type": "application/dns-message"
     };
 
     let newRequest;
@@ -75,8 +147,8 @@ async function resolveDns(request, resolverUrl, requestBodyBuffer, runTimeEnv) {
     ) {
       u.search = runTimeEnv == "worker" && request.method === "POST"
         ? "?dns=" +
-          btoa(String.fromCharCode(...new Uint8Array(requestBodyBuffer)))
-            .replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "")
+        btoa(String.fromCharCode(...new Uint8Array(requestBodyBuffer)))
+          .replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "")
         : u.search;
       // console.debug("buf length:", requestBodyBuffer.byteLength);
       // console.debug(u.href);
