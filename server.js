serverless-dns/serverless-dns

commit 8d712dcfcb88e9493e488f5b234787779043697e

fix(node): min-max bounds for dns packet size

server.js
@@ -11,6 +11,8 @@ const tlsOptions = {
   key: TLS_KEY,
   cert: TLS_CRT,
 };
+const MinDNSPacketSize = 12 + 5;
+const MaxDNSPacketSize = 4096;
 
 const tServer = tls.createServer(tlsOptions, serveTLS).listen(
   TLS_PORT,
@@ -40,7 +42,7 @@ function serveTLS(socket) {
   let qlBuf = Buffer.allocUnsafe(2).fill(0);
   let qlBufPtr = 0;
 
-  socket.on("data", /** @param {ArrayBuffer} chunk */ (chunk) => {
+  socket.on("data", /** @param {Buffer} chunk */ (chunk) => {
     const cl = chunk.byteLength;
     if (cl == 0) return;
     if (cl == 1) {
@@ -60,8 +62,13 @@ function serveTLS(socket) {
 
     const ql = qlBuf.readUInt16BE() || chunk.slice(0, 2).readUInt16BE();
     // console.debug(`q len = ${ql}`);
+    if (ql < MinDNSPacketSize || ql > MaxDNSPacketSize) {
+      console.warn(`TCP query length out of [min, max] bounds: ${ql}`);
+      socket.destroy();
+      return;
+    }
 
-    const q = qlBuf.readUInt16BE() ? chunk : chunk.slice(2, ql + 2);
+    const q = qlBuf.readUInt16BE() ? chunk : chunk.slice(2);
     // console.debug(`Read q:`, q);
     qlBuf.fill(0);
 
@@ -82,36 +89,23 @@ function serveTLS(socket) {
 }
 
 /**
- * Services a DNS over HTTPS connection
- * @param {IncomingMessage} req
- * @param {ServerResponse} res
- */
-async function serveHTTPS(req, res) {
-  const buffers = [];
-  for await (const chunk of req) {
-    buffers.push(chunk);
-  }
-  const q = Buffer.concat(buffers);
-
-  // console.debug("-> HTTPS req");
-  handleHTTPRequest(q, req, res);
-}
-
-/**
  * @param {Buffer} q
  * @param {tls.TLSSocket} socket
  */
 async function handleTCPQuery(q, socket) {
   try {
+    // const t1 = Date.now(); // debug
     const r = await resolveQuery(q, socket.servername);
     const rlBuf = encodeUint8ArrayBE(r.byteLength, 2);
     if (!socket.destroyed) {
       const wrote = socket.write(new Uint8Array([...rlBuf, ...r]));
       if (!wrote) console.error(`res write incomplete: < ${r.byteLength + 2}`);
+      // console.debug("processing time t-q =", Date.now() - t1);
     }
   } catch (e) {
     console.warn(e);
-    if (!socket.destroyed) socket.write((new Uint8Array(2)).fill(0));
+  } finally {
+    if (!socket.destroyed) socket.destroy();
   }
 }
 
@@ -125,18 +119,41 @@ async function resolveQuery(q, sni) {
     ? ["", sni]
     : [sni.split(".", 1)[0], sni.slice(sni.indexOf(".") + 1)];
 
-  const response = await handleRequest({
-    request: new Request(`https://${host}/${flag}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/dns-message",
-        "content-length": q.byteLength.toString(),
+  const r = await handleRequest({
+    request: new Request(
+      `https://${host}/${flag}?dns=${q.toString("base64url")}`,
+      {
+        method: "GET",
+        headers: {
+          "Accept": "application/dns-message",
+        },
       },
-      body: q,
-    }),
+    ),
   });
 
-  return new Uint8Array(await response.arrayBuffer());
+  return new Uint8Array(await r.arrayBuffer());
+}
+
+/**
+ * Services a DNS over HTTPS connection
+ * @param {IncomingMessage} req
+ * @param {ServerResponse} res
+ */
+async function serveHTTPS(req, res) {
+  const buffers = [];
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+  const q = Buffer.concat(buffers);
+
+  if (q.byteLength > MaxDNSPacketSize) {
+    console.warn(`HTTP req body too large: ${q.byteLength}`);
+    res.end();
+    return;
+  }
+
+  // console.debug("-> HTTPS req", q.byteLength);
+  handleHTTPRequest(q, req, res);
 }
 
 /**
@@ -145,16 +162,24 @@ async function resolveQuery(q, sni) {
  * @param {ServerResponse} res
  */
 async function handleHTTPRequest(q, req, res) {
-  const fRequest = new Request(
-    new URL(req.url, `https://${req.headers.host}`),
-    {
-      ...req,
-      body: req.method.toUpperCase() == "POST" ? q : null,
-    },
-  );
+  try {
+    // const t1 = Date.now(); // debug
+    const r = await handleRequest({
+      request: new Request(
+        new URL(req.url, `https://${req.headers.host}`),
+        {
+          ...req,
+          body: req.method.toUpperCase() == "POST" ? q : null,
+        },
+      ),
+    });
 
-  const r = await handleRequest({ request: fRequest });
-
-  res.writeHead(r.status, r.statusText, r.headers);
-  res.end(Buffer.from(await r.arrayBuffer()));
+    res.writeHead(r.status, r.statusText, r.headers);
+    res.end(Buffer.from(await r.arrayBuffer()));
+    // console.debug("processing time h-q =", Date.now() - t1);
+  } catch (e) {
+    console.warn(e);
+  } finally {
+    res.end();
+  }
 }

helpers/node/config.js
@@ -1,11 +1,10 @@
-import { config as dotEnvConfig } from "dotenv";
 import { atob } from "buffer";
 import fetch, { Headers, Request, Response } from "node-fetch";
 import { getTLSfromEnv } from "./util.js";
 
 // Load env variables from .env file to process.env (if file exists)
 // NOTE: this won't overwrite existing
-if (process.env.NODE_ENV !== "production") dotEnvConfig();
+if (process.env.NODE_ENV !== "production") (await import("dotenv")).config();
 process.env.RUNTIME_ENV = "node";
 
 if (!globalThis.fetch) {
@@ -21,7 +20,7 @@ if (!globalThis.atob) {
   globalThis.atob = atob;
 }
 
-const TLS_CN = process.env.TLS_COMMON_NAME ?? "";
+const TLS_CN = process.env.TLS_CN ?? "";
 const TLS_CRT_KEY = eval(`process.env.TLS_${TLS_CN}`);
 
 export const [TLS_KEY, TLS_CRT] =

plugin.js
@@ -8,9 +8,11 @@
 
 import { CommandControl } from "@serverless-dns/command-control";
 import { UserOperation } from "@serverless-dns/basic";
-import { DNSBlock } from "@serverless-dns/dns-operation";
-import { DNSResolver } from "@serverless-dns/dns-operation";
-import { DNSResponseBlock } from "@serverless-dns/dns-operation";
+import {
+  DNSBlock,
+  DNSResolver,
+  DNSResponseBlock,
+} from "@serverless-dns/dns-operation";
 
 const commandControl = new CommandControl();
 const userOperation = new UserOperation();

commit bef0a44598e6530f4c961e7d7c21e79d032c68eb

fix(node): tcp pipelining regression; check http body min len
DoT query pipelining regression was introduced in
8d712dcfcb88e9493e488f5b234787779043697e

server.js
@@ -11,8 +11,8 @@ const tlsOptions = {
   key: TLS_KEY,
   cert: TLS_CRT,
 };
-const MinDNSPacketSize = 12 + 5;
-const MaxDNSPacketSize = 4096;
+const minDNSPacketSize = 12 + 5;
+const maxDNSPacketSize = 4096;
 
 const tServer = tls.createServer(tlsOptions, serveTLS).listen(
   TLS_PORT,
@@ -63,7 +63,7 @@ function serveTLS(socket) {
 
     const ql = qlBuf.readUInt16BE() || chunk.slice(0, 2).readUInt16BE();
     // console.debug(`q len = ${ql}`);
-    if (ql < MinDNSPacketSize || ql > MaxDNSPacketSize) {
+    if (ql < minDNSPacketSize || ql > maxDNSPacketSize) {
       console.warn(`TCP query length out of [min, max] bounds: ${ql}`);
       socket.destroy();
       return;
@@ -105,7 +105,7 @@ async function handleTCPQuery(q, socket) {
     }
   } catch (e) {
     console.warn(e);
-  } finally {
+    // Only close socket on error, else it would break pipelining of queries.
     if (!socket.destroyed) socket.destroy();
   }
 }
@@ -149,24 +149,27 @@ async function serveHTTPS(req, res) {
   for await (const chunk of req) {
     buffers.push(chunk);
   }
-  const q = Buffer.concat(buffers);
+  const b = Buffer.concat(buffers);
+  const bl = b.byteLength;
 
-  if (q.byteLength > MaxDNSPacketSize) {
-    console.warn(`HTTP req body too large: ${q.byteLength}`);
+  if (
+    req.method == "POST" && (bl < minDNSPacketSize || bl > maxDNSPacketSize)
+  ) {
+    console.warn(`HTTP req body length out of [min, max] bounds: ${bl}`);
     res.end();
     return;
   }
 
-  // console.debug("-> HTTPS req", q.byteLength);
-  handleHTTPRequest(q, req, res);
+  // console.debug("-> HTTPS req", req.method, bl);
+  handleHTTPRequest(b, req, res);
 }
 
 /**
- * @param {Buffer} q - Request body
+ * @param {Buffer} b - Request body
  * @param {IncomingMessage} req
  * @param {ServerResponse} res
  */
-async function handleHTTPRequest(q, req, res) {
+async function handleHTTPRequest(b, req, res) {
   try {
     // const t1 = Date.now(); // debug
     const fReq = new Request(
@@ -176,7 +179,7 @@ async function handleHTTPRequest(q, req, res) {
         // properties, especially of "hidden" Symbol values!? like "headers"?
         ...req,
         headers: req.headers,
-        body: req.method.toUpperCase() == "POST" ? q : null,
+        body: req.method.toUpperCase() == "POST" ? b : null,
       },
     );
     const fRes = await handleRequest({ request: fReq });