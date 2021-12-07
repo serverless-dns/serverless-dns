0c74f5e9475b01bde9c01d8693b10148c727adba
ignoramous <ignoramous@users.noreply.github.com>
2021-12-07 12:22:15 +0530

    MPL license header

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021 RethinkDNS and its authors.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { TLS_CRT, TLS_KEY } from "./helpers/node/config.js";
 import * as tls from "tls";
 // import { IncomingMessage, ServerResponse } from "http";
