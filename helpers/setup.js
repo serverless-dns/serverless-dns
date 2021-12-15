eb2827427737ba35a6ae57cb24c99ffa79054360
Murtaza Aliakbar <murtaza@live.in>
2021-12-16 02:54:59 +0530

    mkswap args must be an array

--- helpers/setup.js
+++ helpers/setup.js
@@ -1,16 +1,23 @@
-import { spawnSync } from 'child_process';
+/*
+ * Copyright (c) 2021 RethinkDNS and its authors.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import { spawnSync } from 'child_process'
 import * as log from './log.js'
 
-const swapfile = 'swap__';
-const swapsize = '128M';
+const swapfile = 'swap__'
+const swapsize = '128M'
 
 // linuxize.com/post/create-a-linux-swap-file
 export function mkswap() {
     return !hasswap() &&
         sh('fallocate', ['-l', swapsize, swapfile]) &&
         sh('chmod', ['600', swapfile]) &&
-        sh('mkswap', swapfile) &&
-        sh('swapon', swapfile) &&
+        sh('mkswap', [swapfile]) &&
+        sh('swapon', [swapfile]) &&
         sh('sysctl', 'vm.swappiness=25')
 }
 
@@ -22,21 +29,22 @@ export function rmswap() {
 
 // stackoverflow.com/a/53222213
 function hasswap() {
-    return sh('test' ['-e', swapfile]);
+    return sh('test' ['-e', swapfile])
 }
 
 function sh(cmd, args) {
-    if (!cmd) return;
-    args = args || [];
+    if (!cmd) return false
+    args = args || []
     const opts = {
         cwd: '/',
         uid: 0,
         shell: true,
+        encoding: 'utf8',
     }
-    const proc = spawnSync(cmd, args, opts);
-    if (proc.error) log.i(cmd, args, opts, "error", proc.error);
-    if (proc.stderr) log.e(cmd, args, opts, proc.stderr);
-    if (proc.stdout) log.g(proc.stdout);
-    return (proc.status === 0);
+    const proc = spawnSync(cmd, args, opts)
+    if (proc.error) log.i(cmd, args, opts, "error", proc.error)
+    if (proc.stderr) log.e(cmd, args, opts, proc.stderr)
+    if (proc.stdout) log.g(proc.stdout)
+    return (proc.status === 0)
 }
 
--- server.js
+++ server.js
@@ -5,7 +5,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
 import net, { isIPv6, Socket } from "net";
 import { V1ProxyProtocol } from "proxy-protocol-js";
 import * as tls from "tls";
