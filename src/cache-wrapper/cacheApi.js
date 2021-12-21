c15a738bf377413b47d0af0c7ca969b55cf38e83
santhosh-ponnusamy <67677652+santhosh-ponnusamy@users.noreply.github.com>
2021-12-21 20:56:57 +0530

    cacheapi check for workers

--- src/cache-wrapper/cacheApi.js
+++ src/cache-wrapper/cacheApi.js
@@ -5,17 +5,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
+import * as envutil from "../helpers/envutil.js";
 export class CacheApi {
-  constructor() {
-    this.wCache = caches.default;
-  }
-
   async get(url) {
-    return await this.wCache.match(url);
+    if (envutil.isWorkers()) {
+      return await caches.default.match(url);
+    }
+    return false;
   }
 
-  put(url, response) {   
-    this.wCache.put(url, response);
+  put(url, response) {
+    if (envutil.isWorkers()) {
+      return caches.default.put(url, response);
+    }
+    return false;
   }
 }
