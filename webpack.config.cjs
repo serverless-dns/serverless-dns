548106259684f9021d2e70edd1ebb7172002abd4
Murtaza Aliakbar <murtaza@live.in>
2022-10-21 11:23:33 +0530

    build: webpack5 node-polyfill plugin for workers

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 // developers.cloudflare.com/workers/cli-wrangler/configuration#modules
 // archive.is/FDky9
@@ -9,21 +10,24 @@ module.exports = {
   // enable devtool in development
   // devtool: 'eval-cheap-module-source-map',
 
+  // gist.github.com/ef4/d2cf5672a93cf241fd47c020b9b3066a
   resolve: {
     fallback: {
-        // buffer polyfill: archive.is/7OBM7
-        buffer: require.resolve('buffer/'),
+      // buffer polyfill: archive.is/7OBM7
+      buffer: require.resolve("buffer/"),
     },
   },
 
   plugins: [
     new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
+      Buffer: ["buffer", "Buffer"],
     }),
     new webpack.IgnorePlugin({
       resourceRegExp:
         /(^dgram$)|(^http2$)|(\/deno\/.*\.ts$)|(.*-deno\.ts$)|(.*\.deno\.ts$)|(\/node\/.*\.js$)|(.*-node\.js$)|(.*\.node\.js$)/,
     }),
+    // stackoverflow.com/a/65556946
+    new NodePolyfillPlugin(),
   ],
 
   optimization: {
