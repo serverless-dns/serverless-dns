a8939c7991e8b6f947d04640900b2acb7f2dfe23
Jake Champion <jchampion@fastly.com>
2022-12-30 20:38:32 +0000

    feat: Add support for Fastly Compute@Edge

---
+++ src/server-fastly.js
@@ -0,0 +1,44 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="@fastly/js-compute" />
+
+/*
+ * Copyright (c) 2021 RethinkDNS and its authors.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import "./core/fastly/config.js";
+import { handleRequest } from "./core/doh.js";
+import * as system from "./system.js";
+import * as util from "./commons/util.js";
+
+import { allowDynamicBackends } from "fastly:experimental";
+
+allowDynamicBackends(true);
+
+addEventListener("fetch", (event) => {
+  return serveDoh(event);
+});
+
+async function serveDoh(event) {
+  // on Fastly, the network-context is only available in an event listener
+  // and so, publish system prepare from here instead of from main which
+  // runs in global-scope.
+  system.pub("prepare");
+
+  const fetchEvent = util.mkFetchEvent(
+    event.request,
+    null,
+    event.waitUntil.bind(event)
+  );
+
+  try {
+    await system.when("go");
+    return await handleRequest(fetchEvent);
+  } catch (e) {
+    console.error("server", "serveDoh err", e);
+    return util.respond405();
+  }
+}
