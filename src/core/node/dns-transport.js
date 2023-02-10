c3a8b253a3e62673af555c9e4fb2a69b7216684a
Murtaza Aliakbar <murtaza@live.in>
2023-02-11 02:00:17 +0530

    dns/transport: attempt udp disconnect before close

--- src/core/node/dns-transport.js
+++ src/core/node/dns-transport.js
@@ -143,6 +143,7 @@ export class Transport {
     // the socket is expected to not have any error-listeners
     // so we add one just in case to avoid unhandled errors
     sock.on("error", util.stub);
+    if (sock) util.safeBox(() => sock.disconnect());
     if (sock) util.safeBox(() => sock.close());
   }
 }
