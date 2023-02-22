e3f2a1ba4da99f088cba09ef8aa17e8f2afc070e
Murtaza Aliakbar <murtaza@live.in>
2023-02-22 19:39:38 +0530

    dns-op/prefilter: rmv unused import

--- src/plugins/dns-op/prefilter.js
+++ src/plugins/dns-op/prefilter.js
@@ -5,7 +5,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import * as rdnsutil from "../rdns-util.js";
 import * as dnsutil from "../../commons/dnsutil.js";
 import * as util from "../../commons/util.js";
 import * as pres from "../plugin-response.js";
