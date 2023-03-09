55267fd7d4e8be95bd9a035618fd821ab9063783
Spencer Comfort <109806759+GiddyGoatGaming@users.noreply.github.com>
2023-03-09 12:29:24 -0500

    dns-op: incr dnscachesize to 30000 (#118)

--- src/commons/dnsutil.js
+++ src/commons/dnsutil.js
@@ -21,7 +21,7 @@ export const maxDNSPacketSize = 4096;
 // TODO: move _dns* related settings to env
 const _dnsCloudflareSec4 = "1.1.1.2";
 const _dnsFly6 = "fdaa::3";
-const _dnsCacheSize = 20000;
+const _dnsCacheSize = 30000;
 
 const _minRequestTimeout = 4000; // 4s
 const _maxRequestTimeout = 30000; // 30s
