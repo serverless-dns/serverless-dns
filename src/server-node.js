db6b38f112b799859670b102273690477bc4ec62
Murtaza Aliakbar <murtaza@live.in>
2023-06-04 04:22:59 +0530

    node: tune adjs based on inference from load tests

--- src/server-node.js
+++ src/server-node.js
@@ -68,7 +68,7 @@ const listeners = { connmap: [], servers: [] };
 const stats = new Stats();
 const tlsSessions = new LfuCache("tlsSessions", 10000);
 const cpucount = os.cpus().length || 1;
-const adjPeriodSec = 15;
+const adjPeriodSec = 5;
 const adjTimer = util.repeat(adjPeriodSec * 1000, adjustMaxConns);
 /** @type {memwatch.HeapDiff} */
 let heapdiff = null;
@@ -1002,12 +1002,14 @@ function adjustMaxConns(n) {
   if (n == null) {
     // determine n based on load-avg
     n = maxc;
-    if (avg1 > 90) {
+    if (avg1 > 100) {
       n = minc;
-    } else if (avg1 > 80 || avg5 > 80) {
+    } else if (avg1 > 90 || avg5 > 80) {
       n = Math.max((n * 0.2) | 0, minc);
-    } else if (avg1 > 70 || avg5 > 70) {
+    } else if (avg1 > 80 || avg5 > 75) {
       n = Math.max((n * 0.4) | 0, minc);
+    } else if (avg1 > 70) {
+      n = Math.max((n * 0.6) | 0, minc);
     } else {
       // reset; n reverting back to maxconns
       adj = 0;
