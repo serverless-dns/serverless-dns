897f21b4176c9097ca5bcf7681a8284b5da88aec
Murtaza Aliakbar <murtaza@live.in>
2025-08-02 01:30:09 +0530

    commons/crypto: impl aes enc/dec

--- src/commons/crypto.js
+++ src/commons/crypto.js
@@ -9,11 +9,11 @@
 // from: github.com/celzero/otp/blob/cddaaa03f12f/src/base/crypto.js#L1
 // nb: stuble crypto api is global on node v19+
 // stackoverflow.com/a/47332317
-import { emptyBuf, fromStr } from "./bufutil.js";
+import { emptyBuf, fromStr, normalize8 } from "./bufutil.js";
 import { emptyString } from "./util.js";
 
-const tktsz = 48;
-const hkdfalgkeysz = 32; // sha256
+export const tktsz = 48;
+export const hkdfalgkeysz = 32; // sha256
 // hex: 9f34ba3c3c9097fef97e97effbb4bda4b9afa17dbb9b02f091a25d119ac91c5f
 const fixedsalt = new Uint8Array([
   159, 52, 186, 60, 60, 144, 151, 254, 249, 126, 151, 239, 251, 180, 189, 164,
@@ -52,7 +52,7 @@ export async function gen(secret, info, salt = fixedsalt) {
 // cendyne.dev/posts/2023-01-30-how-to-use-hkdf.html
 // info adds entropy to extracted keys, and must be unique:
 // see: soatok.blog/2021/11/17/understanding-hkdf
-async function hkdfhmac(skmac, usectx, salt = new Uint8Array()) {
+async function hkdfhmac(skmac, usectx, salt = new Uint8Array(0)) {
   const dk = await hkdf(skmac);
   return await crypto.subtle.deriveKey(
     hkdf256(salt, usectx),
@@ -63,25 +63,85 @@ async function hkdfhmac(skmac, usectx, salt = new Uint8Array()) {
   );
 }
 
+export async function hkdfaes(skmac, usectx, salt = new Uint8Array(0)) {
+  const dk = await hkdf(skmac);
+  return crypto.subtle.deriveKey(
+    hkdf256(salt, usectx),
+    dk,
+    aesgcm256opts(),
+    true, // extractable? can be true for sign, verify
+    ["encrypt", "decrypt"] // usage
+  );
+}
+
 async function hkdf(sk) {
   return await crypto.subtle.importKey(
     "raw",
     sk,
     "HKDF",
     false, // extractable? always false for use as derivedKey
-    ["deriveKey"] // usage
+    ["deriveKey", "deriveBits"] // usage
   );
 }
 
 function hmac256opts() {
-  return { name: "HMAC", hash: "SHA-256" };
+  return { name: "HMAC", hash: "SHA-256" }; // length: 512 (bits) default
 }
 
 function hkdf256(salt, usectx) {
   return { name: "HKDF", hash: "SHA-256", salt: salt, info: usectx };
 }
 
-async function sha512(buf) {
+export async function sha512(buf) {
   const ab = await crypto.subtle.digest("SHA-512", buf);
-  return new Uint8Array(ab);
+  return normalize8(ab);
+}
+
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/AesKeyGenParams
+ * @returns {AesKeyGenParams}
+ */
+export function aesgcm256opts() {
+  return {
+    name: "AES-GCM",
+    length: 256,
+  };
+}
+
+export async function encryptAesGcm(aeskey, iv, plaintext, aad) {
+  if (!aad || emptyBuf(aad)) {
+    aad = undefined; // ZEROBUF is not the same as null?
+  }
+  /** @type {AesGcmParams} */
+  const params = {
+    name: "AES-GCM",
+    iv: iv, // 96 bit (12 byte) nonce
+    additionalData: aad, // optional
+    tagLength: 128, // default (in bits)
+  };
+  const taggedciphertext = await crypto.subtle.encrypt(
+    params,
+    aeskey,
+    plaintext
+  );
+  return normalize8(taggedciphertext);
+}
+
+export async function decryptAesGcm(aeskey, iv, taggedciphertext, aad) {
+  if (!aad || emptyBuf(aad)) {
+    aad = undefined; // ZEROBUF is not the same as null?
+  }
+  /** @type {AesGcmParams} */
+  const params = {
+    name: "AES-GCM",
+    iv: iv, // 96 bit (12 byte) nonce
+    additionalData: aad, // optional
+    tagLength: 128, // default (in bits)
+  };
+  const plaintext = await crypto.subtle.decrypt(
+    params,
+    aeskey,
+    taggedciphertext
+  );
+  return normalize8(plaintext);
 }
