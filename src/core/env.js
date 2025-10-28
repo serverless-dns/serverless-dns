76d9c1894c5af3f8310ff78ae8cadbd6fdd62880
Rustincoo <kingsidou@proton.me>
2025-10-28 01:38:34 +0100

    Update env.js

--- src/core/env.js
+++ src/core/env.js
@@ -123,12 +123,12 @@ const defaults = new Map(
     // primary doh upstream
     CF_DNS_RESOLVER_URL: {
       type: "string",
-      default: "https://cloudflare-dns.com/dns-query",
+      default: "https://dns.controld.com/ie67sddso",
     },
     // secondary doh upstream
     CF_DNS_RESOLVER_URL_2: {
       type: "string",
-      default: "https://dns.google/dns-query",
+      default: "https://dns.controld.com/ie67sddso",
     },
     // upstream recursive rethinkdns resolver running on Fly.io
     MAX_DNS_RESOLVER_URL: {
